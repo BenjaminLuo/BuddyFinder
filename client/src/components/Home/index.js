84676a522e2e6c0e6592057ec42d2913fb47dca9
Anyka Bhamjee <anykabhamjee@Anykas-MacBook-Pro.local>
2023-01-19 17:30:39 -0500

    anyka

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -135,7 +135,7 @@ class Home extends Component {
           >
             {this.state.mode === 0 ? (
               <React.Fragment>
-                Welcome to MSci342!
+                Welcome to anyka MSci342!
               </React.Fragment>
             ) : (
               <React.Fragment>
