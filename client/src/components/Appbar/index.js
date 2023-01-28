18f492e14e32cde7093db9a49bcc8c2a653e4929
AACR1752 <aa5chowd@uwaterloo.ca>
2023-01-28 12:58:30 -0500

    Appbar

---
+++ client/src/components/Appbar/index.js
@@ -0,0 +1,119 @@
+import * as React from 'react';
+
+import Typography from "@material-ui/core/Typography";
+import Link from '@material-ui/core/Link';
+import history from '../Navigation/history';
+
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/core/Menu';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import MenuItem from '@material-ui/core/MenuItem';
+//import AdbIcon from '@material-ui/core/Adb';
+
+const Appbar = () => {
+
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+  
+  
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  
+    const handleOpenNavMenu = (event) => {
+      setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+      setAnchorElUser(event.currentTarget);
+    };
+  
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
+  
+    const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+    };
+
+  return (
+
+
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+           
+           <Button
+             // key={page}
+             onClick={() => history.push('/')}
+             sx={{ my: 2, color: 'white', display: 'block' }}
+           >
+             Landing
+           </Button>
+         
+       </Box>
+
+       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+           
+           <Button
+             // key={page}
+             onClick={() => history.push('/Home')}
+             sx={{ my: 2, color: 'white', display: 'block' }}
+           >
+            Home
+           </Button>
+         
+       </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} >
+           
+              <Button
+                // key={page}
+                onClick={() => history.push('/Statistics')}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Stats
+              </Button>
+            
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+           
+           <Button
+             // key={page}
+             onClick={() => history.push('/Discussion')}
+             sx={{ my: 2, color: 'white', display: 'block' }}
+           >
+             Discussion
+           </Button>
+
+           </Box>
+
+           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+           
+           <Button
+             // key={page}
+             onClick={() => history.push('/Matching')}
+             sx={{ my: 2, color: 'white', display: 'block' }}
+           >
+             Matching
+           </Button>
+         
+       </Box>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+
+    
+  )
+}
+
+export default Appbar;
--- client/src/components/Navigation/PrivateRoute.js
+++ client/src/components/Navigation/PrivateRoute.js
@@ -3,6 +3,12 @@ import { Router, Switch, Route } from "react-router-dom";
 import Home from '../Home';
 import history from './history';
 
+import Statistics from '../Statistics';
+import Discussion from '../Discussion';
+import Mactching from '../Matching';
+import SignIn from '../SignIn';
+import Landing from '../Landing';
+
 export default function PrivateRoute({
   //authenticated,
   //...rest
@@ -12,6 +18,13 @@ export default function PrivateRoute({
     <Router history={history}>
       <Switch>
       <Route path="/" exact component={Home} />
+
+      <Route path="/Statistics" exact component={Statistics} />
+      <Route path="/Discussion" exact component={Discussion} /> 
+      <Route path="/Matching" exact component={Mactching} />     
+      <Route path="/SignIn" exact component={SignIn} />
+      <Route path="/" exact component={Landing} />
+
       </Switch>
     </Router>
   );
