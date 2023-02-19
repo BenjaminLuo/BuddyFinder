33f1cb7590275f90fa09417bd2944b0f53897321
epheitea <ephei@outlook.com>
2023-02-19 08:12:02 +0700

    Added Contact Us Page (UI only)

--- client/src/components/Navigation/PrivateRoute.js
+++ client/src/components/Navigation/PrivateRoute.js
@@ -7,6 +7,8 @@ import Statistics from '../Statistics';
 import Discussion from '../Discussion';
 import Mactching from '../Matching';
 import Landing from '../Landing';
+import Contact from '../Contact';
+import Settings from '../Settings';
 
 export default function PrivateRoute({
   //authenticated,
@@ -21,7 +23,8 @@ export default function PrivateRoute({
         <Route path="/Statistics" exact component={Statistics} />
         <Route path="/Discussion" exact component={Discussion} />
         <Route path="/Matching" exact component={Mactching} />
-
+        <Route path="/Contact" exact component={Contact} />
+        <Route path="/Settings" exact component={Settings} />
       </Switch>
     </Router>
   );
