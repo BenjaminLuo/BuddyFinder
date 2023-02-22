310977166ca2755321a4b7b83971cae4fa3a7941
epheitea <ephei@outlook.com>
2023-02-22 09:19:33 +0700

    Finished Statistics page (sprint 1)

--- client/src/components/Statistics/index.js
+++ client/src/components/Statistics/index.js
@@ -35,37 +35,83 @@ const useStyles = makeStyles((theme) => {
 })
 
 
+const goals = [
+  {
+    id: 1,
+    goal: "To pass 3A with 86%+",
+    date: "",
+    completed: false
+  },
+  {
+    id: 2,
+    goal: "To go on exchange",
+    date: "",
+    completed: false
+  },
+  {
+    id: 3,
+    goal: "To finish sprint 1 on time",
+    date: "",
+    completed: true
+  }
+]
+
+
 export default function Statistics(props) {
   const classes = useStyles();
 
-  const [extraToDoItems, updateExtraToDoItems] = React.useState(<ToDoItem />)
-  const [goalObject, updateGoalObject] = React.useState([
-    {
-      id: 1,
-      goal: "To pass 3A with 86%+",
-      date: "",
-      completed: false
-    },
-    {
-      id: 2,
-      goal: "To go on exchange",
-      date: "",
-      completed: false
-    },
-    {
-      id: 3,
-      goal: "To finish sprint 1 on time",
-      date: "",
-      completed: true
-    }
-  ]);
+  const [goalObject, updateGoalObject] = React.useState(goals);
+  const [nextID, upID] = React.useState(goals.length + 1);
 
 
-
-  function AddToDoItem(props) {
-    updateExtraToDoItems([extraToDoItems, <ToDoItem />]);
+  const handleChange = (e) => {
+    goalObject[goalObject.findIndex(item => "tf" + item.id == e.target.id)].goal = e.target.value
+    updateGoalObject([...goalObject])
   }
 
+  // Find index then remove it
+  const removeItem = (e) => {
+    goalObject[goalObject.findIndex(item => "button" + item.id == e.target.id)].completed = true
+    updateGoalObject(goalObject.filter((value, index) => value.completed !== true))
+  }
+
+  const ToDoItem = (props) => {
+    return (
+      <div id={props.itemID}>
+        <TextField
+          placeholder={'Set a goal'}
+          variant="outlined"
+          value={props.value}
+          id={'tf' + props.itemID}
+          onChange={handleChange}
+          fullWidth
+          size="small"
+          color="primary"
+          style={{ marginBottom: "20px", width: '50%' }} />
+
+        <Button
+          variant="contained"
+          color="primary"
+          id={'button' + props.itemID}
+          onClick={removeItem}
+          style={{ height: '30px', marginLeft: '15px', marginTop: '3px' }}>
+          Done
+        </Button>
+      </div>
+    )
+  }
+
+
+  // Adds another To-Do item
+  const handleAddition = () => {
+    updateGoalObject([...goalObject, {
+      id: nextID,
+      goal: "",
+      date: "",
+      completed: false
+    }])
+    upID(nextID + 1);
+  }
 
   return (
     <Container maxWidth={false} className={classes.page}>
@@ -79,15 +125,15 @@ export default function Statistics(props) {
             My To-Do List
           </Typography>
 
+          {/* Retrieves all existing user goals */}
           {goalObject ? goalObject.map((item) => (
-            <ToDoItem key={item.id} value={item.goal} disabled={true} />
+            item.completed ? '' :
+              ToDoItem({ itemID: item.id.toString(), value: item.goal })
           )) : null}
-
-          {extraToDoItems}
 
           <Button
             type="submit"
-            onClick={AddToDoItem}
+            onClick={(e) => handleAddition(e)}
             variant="contained"
             color="primary"
             style={{ height: '30px', marginTop: '3px' }}>
@@ -112,7 +158,7 @@ export default function Statistics(props) {
             <Table>
               <TableHead style={{ backgroundColor: '#6AAB8E' }}>
                 <TableRow>
-                  <TableCell align="center" colspan={2}>Usage Statistics</TableCell>
+                  <TableCell align="center" colSpan={2}>Usage Statistics</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -147,36 +193,4 @@ const rows = [
   createData('Scheduled Events (hours)', 43),
 ];
 
-const ToDoItem = (props) => {
-  return (
-    <div id={props.key}>
-      <TextField
-        placeholder={'Set a goal'}
-        disabled={props.disabled}
-        focused={!props.disabled}
-        value={props.value}
-        variant="outlined"
-        key={'tf' + props.key}
-        fullWidth
-        size="small"
-        color="primary"
-        style={{ marginBottom: "20px", width: '50%' }} />
 
-      <Button
-        variant="contained"
-        key={'b' + props.key}
-        color="primary"
-        // onClick={RemoveItem(1)}
-        style={{ height: '30px', marginLeft: '15px', marginTop: '3px' }}>
-        Done
-      </Button>
-    </div>
-  )
-}
-
-
-// const RemoveItem = (id) => {
-//   return (
-//     document.getElementById(id).value = ''
-//   )
-// }
