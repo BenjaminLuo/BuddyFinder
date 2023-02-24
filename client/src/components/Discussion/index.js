0d04fe7792532c05843c90d7147a2adf037a395d
Fuad Khan <f237khan@Fuads-MacBook-Pro.local>
2023-02-23 22:56:03 -0500

    Added news feed input. Need to store user data

--- client/src/components/Discussion/index.js
+++ client/src/components/Discussion/index.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import {
   Typography,
   Container,
@@ -22,28 +24,46 @@ const useStyles = makeStyles((theme) => {
   }
 })
 
-export default function Matching(props) {
+const txt1 = document.getElementById('fullWidth');
+const btn1 = document.getElementById('btn1');
+const out1 = document.getElementById('output1');
+
+function fun1() {
+  out1.innerHTML = txt1.value; 
+}
+
+export default function Discussion(props) {
   const classes = useStyles();
 
   return (
     <Container maxWidth={false} className={classes.page}>
 
-            <Grid container spacing={2}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h4" align="center">
+            Discussion Forum
+          </Typography>
+          <hr style={{ align: 'center', width: '10%', borderColor: 'black' }} />
 
-                <Grid item xs={12}>
-                    <Typography gutterBottom variant="h4" align="center">
-                        Discussion Forum
-                    </Typography>
-                    <hr style={{ align: 'center', width: '10%', borderColor: 'black' }} />
+          <h2 style={{ padding: '50px' }} align="left">
+            Drop your latest feed below!
+          </h2>
+        </Grid>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: '100%',
+          }}
+        >
+          <TextField fullWidth label="What's on your mind?" id="fullWidth" />
+        </Box>
 
-                    <Typography style={{ padding: '50px' }} align="center">
-                        Let's connect! (I'll update this text later)
-                    </Typography>
-
-                </Grid>
-
-            </Grid>
-        </Container>
+        <Stack spacing={2} direction="column">
+          <Button id='btn1' variant="outlined">Share</Button>
+        </Stack>
+        <p className='output' id='output1'></p>
+      </Grid>
+    </Container>
   )
 
 }
