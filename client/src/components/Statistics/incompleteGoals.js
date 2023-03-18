525111ae19761fb98955ee9eb20c436fa171070e
epheitea <ephei@outlook.com>
2023-03-19 05:40:45 +0800

    Enabled completed goals to be viewable

---
+++ client/src/components/Statistics/incompleteGoals.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Typography, Button } from '@material-ui/core';
+import { TabPanel } from '../../components/UI/TabPanel';
+
+// Tab menu for goals that are incomplete
+export const IncompleteGoals = ({ tabToggle, goalObject, ToDoItem, handleAddition }) => {
+    return (
+        <TabPanel value={tabToggle} index={0}>
+
+            {/* Retrieves all existing user goals */}
+            {goalObject ? goalObject.map((item) => (
+                item.completed ? '' :
+                    ToDoItem({ itemID: item.id.toString(), value: item.goal })
+            )) : null}
+
+            <Button
+                type="submit"
+                onClick={(e) => handleAddition(e)}
+                variant="contained"
+                color="primary"
+                style={{ height: '30px', marginTop: '3px' }}>
+                <Typography variant="h6">Add another task...</Typography>
+            </Button>
+
+        </TabPanel>
+    );
+}
