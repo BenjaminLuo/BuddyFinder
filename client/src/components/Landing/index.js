61e81ed1cd5f07bca2bce043de50aff7a6af46bc
Anyka Bhamjee <anykabhamjee@Anykas-MacBook-Pro.local>
2023-03-25 15:17:54 -0400

    repush landing

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -9,7 +9,7 @@ export default function Profile() {
 
     return (
         <div align="center" style={{
-            //backgroundcolor: '#5C5D8D',
+            backgroundcolor: '#5C5D8D',
           }}>
             
           <Card
@@ -31,7 +31,7 @@ export default function Profile() {
                 align: 'center',
               }}
             >
-              Buddy Finder
+              Buddy Finder 
             </Typography>
           </Card>
           <Card
@@ -53,7 +53,7 @@ export default function Profile() {
                 align: 'left',
               }}
             >
-              Welcome to Buddy Finder! On our website you'll be able to find other students with similar interests and schedule times to meet them. Get started now!</Typography>
+              Welcome to Buddy Finder! On this website you'll be able to find other students with similar interests and schedule times to meet them. Get started now!</Typography>
           </Card>
         </div>
     )
